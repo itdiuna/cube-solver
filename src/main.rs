6fc5cf24cb712e73fcf6e4a0676d170ccce13995
Pawel Rawski <pawel@itdiuna.com>
2022-01-05 16:08:45 +0100

    Order starting points

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::cmp;
 
 const BOARD_SIZE: usize = 6;
 
+const POINT_ORDER_SIZE: usize = (BOARD_SIZE+1).pow(3)*8;
+
 const BLOCK_SIZE: usize = 4;
 
 const DENSITY_FACTOR: f32 = 4 as f32 / 27 as f32;
@@ -100,7 +102,8 @@ fn main() {
 
 struct Board {
     points: BoardType,
-    neighbours: [[[usize; BOARD_SIZE]; BOARD_SIZE]; BOARD_SIZE]
+    neighbours: [[[usize; BOARD_SIZE]; BOARD_SIZE]; BOARD_SIZE],
+    point_order: [Point; POINT_ORDER_SIZE]
 }
 
 impl fmt::Debug for Board {
@@ -127,7 +130,8 @@ impl Board {
     fn create_empty_board() -> Board {
         let mut board = Board {
             points: [[[false; BOARD_SIZE]; BOARD_SIZE]; BOARD_SIZE],
-            neighbours: [[[6;  BOARD_SIZE]; BOARD_SIZE]; BOARD_SIZE]
+            neighbours: [[[6;  BOARD_SIZE]; BOARD_SIZE]; BOARD_SIZE],
+            point_order: Self::generate()
         };
 
         for x in 0..BOARD_SIZE {
@@ -145,14 +149,33 @@ impl Board {
         board
     }
 
+    fn generate() -> [Point; POINT_ORDER_SIZE] {
+        let initPoint = build_point(0, 0, 0);
+        let mut order = [initPoint; POINT_ORDER_SIZE];
+        for x in 0..BOARD_SIZE {
+            for y in 0..BOARD_SIZE {
+                for z in 0..BOARD_SIZE {
+                    let k = Self::cantorPairing(x, Self::cantorPairing(y, z));
+//                    println!("{:?}: {:?} {:?} {:?}", k, x, y, z);
+                    order[k] = build_point(x, y, z);
+                }
+            }
+        }
+        order
+    }
+
+    fn cantorPairing(k1: usize, k2: usize) -> usize {
+        (k1+k2)*(k1+k2+1)/2 + k2
+    }
+
     fn fill_with(&mut self, block_types: BlockTypes) {
-        self.fill_next(&block_types, 0, &build_point(0, 0, 0));
+        self.fill_next(&block_types, 0, 0);
     }
 
     fn put(&mut self, block: &BlockPosition) -> bool {
         let points = &block.points;
         for point in points {
-            if point.x == BOARD_SIZE || point.y == BOARD_SIZE || point.z == BOARD_SIZE || self.points[point.x][point.y][point.z] { return false; }
+            if point.x >= BOARD_SIZE || point.y >= BOARD_SIZE || point.z >= BOARD_SIZE || self.points[point.x][point.y][point.z] { return false; }
         }
 
         let offsets = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)];
@@ -228,8 +251,8 @@ impl Board {
         return true;
     }
 
-    fn fill_next(&mut self, block_types: &BlockTypes, next_block_type_index: usize, next_point_start: &Point) -> bool {
-        let block = &block_types[next_block_type_index].create_block_position(next_point_start);
+    fn fill_next(&mut self, block_types: &BlockTypes, next_block_type_index: usize, start_point_index: usize) -> bool {
+        let block = &block_types[next_block_type_index].create_block_position(&self.point_order[start_point_index]);
         //println!("check points: {:?}", block);
 
         if !self.put(block) { return false; };
@@ -239,16 +262,13 @@ impl Board {
             return true;
         };
 
+//        println!("{}", start_point_index);
         if self.dense() && self.has_no_holes() {
-            for x in 0..BOARD_SIZE {
-                for y in 0..BOARD_SIZE {
-                    for z in 0..BOARD_SIZE {
-                        for block_type in 0..NUM_OF_BLOCK_TYPES {
-                            if self.fill_next(&block_types, block_type, &build_point(x, y, z)) {
-                                println!("block: {:?}", block);
-                                return true;
-                            }
-                        }
+            for k in (start_point_index+1)..POINT_ORDER_SIZE {
+                for block_type in 0..NUM_OF_BLOCK_TYPES {
+                    if self.fill_next(&block_types, block_type, k) {
+                        println!("block: {:?}", block);
+                        return true;
                     }
                 }
             }
@@ -292,7 +312,7 @@ impl Board {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Point {
     x: usize,
     y: usize,
